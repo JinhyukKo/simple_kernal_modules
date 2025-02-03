3e7113d23f9ade43f6573bf95d32950c7f64db99
JinHyukKo <112324054+JinhyukKo@users.noreply.github.com>
2025-02-03 14:58:49 +0100

    Update createProc.c

--- createProc.c
+++ createProc.c
@@ -1,3 +1,4 @@
+
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -7,39 +8,39 @@
 #define PROC_NAME "hello"
 ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
 static struct file_operations proc_ops = {
-  .owner = THIS_MODULE,
-  .read = proc_read,
-};
+    .owner = THIS_MODULE,
+    .read = proc_read,
+  };
 /* This function is called when the module is loaded. */
 int proc_init(void)
 {
-/* creates the /proc/hello entry */
-proc_create(PROC_NAME, 0666, NULL, &proc_ops);
-return 0;
+    /* creates the /proc/hello entry */
+    proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+    return 0;
 }
 /* This function is called when the module is removed. */
 void proc_exit(void)
 {
-/* removes the /proc/hello entry */
-remove_proc_entry(PROC_NAME, NULL);
+    /* removes the /proc/hello entry */
+    remove_proc_entry(PROC_NAME, NULL);
 }
 
 /* This function is called each time /proc/hello is read */
-ssize_t proc_read(struct file *file, char user *usr_buf,
+ssize_t proc_read(struct file *file, char __user *usr_buf,
 size_t count, loff_t *pos)
 {
-int rv = 0;
-char buffer[BUFFER_SIZE];
-static int completed = 0;
-if (completed) {
-completed = 0;
-return 0;
-}
-completed = 1;
-rv = sprintf(buffer, "Hello World∖n");
-/* copies kernel space buffer to user space usr buf */
-copy_to_user(usr buf, buffer, rv);
-return rv;
+    int rv = 0;
+    char buffer[BUFFER_SIZE];
+    static int completed = 0;
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
+    completed = 1;
+    rv = sprintf(buffer, "Hello World\n");
+    /* copies kernel space buffer to user space usr buf */
+    copy_to_user(usr_buf, buffer, rv);
+    return rv;
 }
 module_init(proc_init);
 module_exit(proc_exit);
